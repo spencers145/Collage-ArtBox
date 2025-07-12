262aaf7ca367fec3583d985bf69f85d4028ea0a5
the-Astra <djs8743@rit.edu>
2025-07-12 17:27:45 -0400

    Proper Marble card rendering

--- upgrades/marble.lua
+++ upgrades/marble.lua
@@ -10,12 +10,11 @@ SMODS.Enhancement({
   config = {
     x_chips = 1,
     extra = {
-      progress=0
+      progress = 0
     }
   },
 
   loc_vars = function(self, info_queue, card)
-    
     return {
       vars = {
         card.ability.x_chips,
@@ -25,20 +24,25 @@ SMODS.Enhancement({
   end,
 
   calculate = function(self, card, context)
-    if context.final_scoring_step and context.cardarea == G.play and not context.debuffed and card.ability.extra.progress <4 then
+    if context.final_scoring_step and context.cardarea == G.play and not context.debuffed and card.ability.extra.progress < 4 then
       G.E_MANAGER:add_event(Event({
         trigger = 'after',
         func = function()
-          card.ability.extra.progress=card.ability.extra.progress+1
-          card.children.center:set_sprite_pos({x = card.ability.extra.progress, y = 1 })
+          card.ability.extra.progress = card.ability.extra.progress + 1
           card:juice_up()
 
           if card.ability.extra.progress == 4 then
-            card.ability.x_chips=2
+            card.ability.x_chips = 2
           end
           return true
-        end 
+        end
       }))
+    end
+  end,
+
+  draw = function(self, card, layer)
+    if (layer == 'card' or layer == 'both') then
+      card.children.center:set_sprite_pos({ x = card.ability.extra.progress, y = 1 })
     end
   end
 })
